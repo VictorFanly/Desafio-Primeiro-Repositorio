6a2328d56a80ca9524dda676fad308afcff5b2a2
VictorFanly <victor.lxxxla@gmail.com>
2022-06-12 18:03:12 -0300

    correção 1.2

---
+++ Projeto3/github-api/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "./components/layout/index"
+import NoSearch from "./components/no-search";
+import Profile from "./components/profile/index"
+import Repositories from "./components/repositories";
+import useGithub from "./hooks/github-hooks";
+
+const App = () => {
+  const { githubState } = useGithub();
+
+  return (
+    <Layout>
+    { githubState.hasUser ? <>
+        { githubState.loading ? (<p> Loading... </p>) : (<> <Profile /> <Repositories /> </>)}
+      </> : <NoSearch />}
+      </Layout>
+  );
+}
+
+export default App;
